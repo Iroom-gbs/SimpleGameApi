f64098063d30c3831addc045808f839ad1f94a39
dayo <dayo05@daum.net>
2022-02-04 02:26:46 +0900

    Make always firing event on main thread

--- src/main/java/com/dayo/simplegameapi/api/Game.kt
+++ src/main/java/com/dayo/simplegameapi/api/Game.kt
@@ -4,6 +4,7 @@ import com.dayo.simplegameapi.data.GameManager
 import com.dayo.simplegameapi.data.RoomInfo
 import com.dayo.simplegameapi.event.GameFinishEvent
 import com.dayo.simplegameapi.event.PlayerFailEvent
+import com.dayo.simplegameapi.util.CoroutineUtil
 import org.bukkit.Bukkit
 import java.util.*
 
@@ -16,13 +17,17 @@ abstract class Game {
 
     open fun playerFailed(room: RoomInfo, player: UUID) {
         GameManager.leftPlayer(player)
-        Bukkit.getPluginManager().callEvent(PlayerFailEvent(player, room))
-        if(GameManager.getCurrentPlayerCount(room) == 0)
-            finish(room)
+        CoroutineUtil.invokeMain {
+            Bukkit.getPluginManager().callEvent(PlayerFailEvent(player, room))
+            if (GameManager.getCurrentPlayerCount(room) == 0)
+                finish(room)
+        }
     }
 
     open fun finish(room: RoomInfo) {
-        GameManager.resetRoom(room)
-        Bukkit.getPluginManager().callEvent(GameFinishEvent(room))
+        CoroutineUtil.invokeMain {
+            GameManager.resetRoom(room)
+            Bukkit.getPluginManager().callEvent(GameFinishEvent(room))
+        }
     }
 }
